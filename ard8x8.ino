2ba9efbc181d2960e2c1e2a495eab24f6d6999eb
abby <abbyskim.work@gmail.com>
2026-02-09 00:36:08 -0500

    Initialize LedControl and create LED loop
    Added LedControl setup and basic loop functionality for LED matrix.

--- ard8x8.ino
+++ ard8x8.ino
@@ -1,7 +1,24 @@
 #include <LedControl.h>
 
+//setup
+LedControl lc = LedControl(12,11,10,1)
+//12 : DIN (send data)
+//11 : CLK (clock)
+//10 : CS/LOAD (tells chip when to read data) 
+//1 : # of matrix modules connected (1)
+
 void setup(){
+  lc.shutdown(0,false); //wake up
+  lc.setIntensity(0,8) //brightness:0-15
+  lc.clearDisplay(0);
 }
 
 void loop(){
+  for (int i = 0; i<8; i++){
+    lc.setLed(0,1,1,true);
+    delay(100);
+}
+
+delay(500);
+lc.clearDisplay(0)
 }
