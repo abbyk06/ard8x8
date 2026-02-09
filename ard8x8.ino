7a76c98bf1bdf1765e4db629f3e20efff639bacf
abby <abbyskim.work@gmail.com>
2026-02-09 00:55:17 -0500

    Implement sound level detection for LED display
    Add sound sensor functionality to control LED matrix display based on sound level.

--- ard8x8.ino
+++ ard8x8.ino
@@ -7,18 +7,30 @@ LedControl lc = LedControl(12,11,10,1)
 //10 : CS/LOAD (tells chip when to read data) 
 //1 : # of matrix modules connected (1)
 
+#define SOUND_PIN A0
+
 void setup(){
   lc.shutdown(0,false); //wake up
   lc.setIntensity(0,8) //brightness:0-15
   lc.clearDisplay(0);
+
+  Serial.begin(9600);
 }
 
-void loop(){
-  for (int i = 0; i<8; i++){
-    lc.setLed(0,1,1,true);
-    delay(100);
-}
+void loop() {
+  int soundValue = analogRead(SOUND_PIN);
 
-delay(500);
-lc.clearDisplay(0)
+  int level = map(soundValue, 0, 1023, 0, 8);
+  level = constrain(level, 0, 8);
+
+  lc.clearDisplay(0);
+
+  // Light based on sound level
+  for (int row = 0; row < level; row++) {
+    for (int col = 0; col < 8; col++) {
+      lc.setLed(0, 7 - row, col, true);
+    }
+  }
+
+  delay(50);
 }
